3e13297923ceb60ce1b39e2cc6b001ae3c76fa9e
cubero <juancarloscuberoalamos@gmail.com>
2020-02-10 09:14:36 +0100

    primera subida

---
+++ Arrays.java
@@ -0,0 +1,14 @@
+package operar_arrays;
+
+import java.lang.reflect.Array;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+
+		UtilsArrays.creaArrayNumerosAzar(10, 1, 10);
+	}
+
+	
+
+}
